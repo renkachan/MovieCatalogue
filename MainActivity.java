dcd4a60b936acd0407739a696ef2261da0044e2d
Renka <arifinrobert2012@gmail.com>
2018-04-16 23:26:57 +0700

    Finishing the model

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,25 @@
 package com.hard.trying.renka.cataloguemovie;
 
+import android.app.LoaderManager;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager
+        .LoaderCallbacks<ArrayList<MovieItem>>, View.OnClickListener {
     Button btnSearch;
+    EditText etMovieName;
     ListView movieLV;
     MovieAdapter adapter;
+    static final String MOVIE_TO_SEARCH= "movie_search";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +32,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         adapter = new MovieAdapter(this);
         adapter.notifyDataSetChanged();
 
+        etMovieName = findViewById(R.id.et_movie_search);
+
+
         movieLV = findViewById(R.id.lv_movie);
 
 
@@ -30,6 +43,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        if (v.getId() == R.id.btn_Search) {
+            String movie = etMovieName.getText().toString();
+            if(TextUtils.isEmpty(movie)) {
+                Toast.makeText(this, "No movie name has been given",Toast.LENGTH_LONG)
+                        .show();
+            } else {
+                    Bundle bundle = new Bundle();
+                    bundle.putString(MOVIE_TO_SEARCH, movie);
+
+                    getLoaderManager().restartLoader(0, bundle, MainActivity.this);
+
+            }
+        }
+
+    }
+
+    @Override
+    public Loader<ArrayList<MovieItem>> onCreateLoader(int id, Bundle args) {
+        return null;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<ArrayList<MovieItem>> loader, ArrayList<MovieItem> data) {
+
+    }
+
+    @Override
+    public void onLoaderReset(Loader<ArrayList<MovieItem>> loader) {
 
     }
 }
